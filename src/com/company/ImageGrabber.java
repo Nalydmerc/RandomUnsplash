b1955fe9d78cb0b33b6e40f4d63d17cc2da65d4b
Nalydmerc <Nalydmerc@gmail.com>
2015-12-21 09:15:38 -0500

    Update

---
+++ src/com/company/ImageGrabber.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Nalydmerc on 12/17/2015.
+ */
+public class ImageGrabber {
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,60 +8,58 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.WinDef.UINT_PTR;
 import com.sun.jna.win32.*;
 
-// Grabs a random image from Unsplash, saves the photo, and sets it as a wallpaper.
-// Mostly pasted together from google resuts.
-// Links:
-// https://source.unsplash.com/
-// http://stackoverflow.com/questions/10292792/getting-image-from-url-java
-// http://stackoverflow.com/questions/4750372/can-i-change-my-windows-desktop-wallpaper-programmatically-in-java-groovy
-// https://github.com/java-native-access/jna
+import static java.util.concurrent.TimeUnit.*;
+
+
+/**
+ * @author Nalydmerc
+ *
+ * Grabs a random image from Unsplash, saves the photo, and sets it as a wallpaper.
+ * Mostly pasted together from google resuts.
+ * Links:
+ *  https://source.unsplash.com/
+ *  http://stackoverflow.com/questions/10292792/getting-image-from-url-java
+ *  http://stackoverflow.com/questions/4750372/can-i-change-my-windows-desktop-wallpaper-programmatically-in-java-groovy
+ *  https://github.com/java-native-access/jna
+ */
+
 
 public class Main {
 
+    private static final ScheduledExecutorService sch = Executors.newScheduledThreadPool(1);
+
     public static void main(String[] args) {
-
-        String filename = "";
-
-        try {
-            URL url = new URL("https://source.unsplash.com/category/buildings/1920x1080");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.getResponseCode();
-            String urlString = con.getURL().toString();
-
-            urlString = urlString.replace("&w=1920", "");
-            urlString = urlString.replace("&h=1080", "");
-            urlString = urlString.replace("&crop=entropy", "");
-            urlString = urlString.replace("&fit=crop", "");
-            //urlString = urlString.replace("&fm=jpg", "&fm=png");
-
-            url = new URL(urlString);
-
-            filename = "C:\\Users\\intern\\Pictures\\" + LocalDate.now().toString() + ".jpg";
-
-            File file = new File(filename);
-            if (!file.exists()) {
-                file.createNewFile();
+        final Runnable GrabMeFreshPaper = new Runnable() {
+            @Override
+            public void run() {
+                String filename = "C:\\Users\\intern\\Pictures\\" + LocalDate.now().toString() + ".jpg";
+                BufferedImage unsplash = grabUnsplashPhoto();
+                savePhoto(unsplash, filename);
+                setWallpaper(filename);
             }
+        };
 
-            BufferedImage image = ImageIO.read(url);
-            ImageIO.write(ImageIO.read(url), "jpg", file);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        long delay = (c.getTimeInMillis()-System.currentTimeMillis());
 
-        SPI.INSTANCE.SystemParametersInfo(
-                new UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
-                new UINT_PTR(0),
-                filename,
-                new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
+        int delayMinutes = (int) delay/60000;
+
+        sch.scheduleAtFixedRate(GrabMeFreshPaper, 0, 1440, MINUTES);
     }
 
         public interface SPI extends StdCallLibrary {
@@ -83,5 +81,65 @@ public class Main {
                 String pvParam,
                 UINT_PTR fWinIni
         );
+    }
+
+    public static void setWallpaper(String path) {
+        SPI.INSTANCE.SystemParametersInfo(
+                new UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
+                new UINT_PTR(0),
+                path,
+                new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
+    }
+
+    public static void savePhoto(BufferedImage image, String path) {
+        try {
+            File file = new File(path);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            ImageIO.write(image, "jpg", file);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BufferedImage grabUnsplashPhoto() {
+        try {
+
+            String [] urlChoices = {
+                    "https://source.unsplash.com/category/buildings/1920x1080",
+                    "https://source.unsplash.com/category/nature/1920x1080"
+            };
+
+            String filename = "";
+
+            Random rand = new Random();
+            int i = rand.nextInt(urlChoices.length);
+            String urlString = urlChoices[i];
+            System.out.print(i);
+
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.getResponseCode();
+            urlString = con.getURL().toString();
+
+            urlString = urlString.replace("&w=1920", "");
+            urlString = urlString.replace("&h=1080", "");
+            urlString = urlString.replace("&crop=entropy", "");
+            urlString = urlString.replace("&fit=crop", "");
+
+            System.out.println(urlString);
+
+            url = new URL(urlString);
+
+            return ImageIO.read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
